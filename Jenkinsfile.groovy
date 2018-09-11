45b49acf7eac27a89461f6a5aa696e125c423aa2
Adrian Jetzer <adrian.jetzer(a)gmail.com>
2018-09-11 16:07:22 +0200

    change to AdrianJetzer

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,7 +12,7 @@ podTemplate(label: 'mypod', containers: [
     node('mypod') {
 
         stage('checkout & unit tests & build') {
-            git url: 'https://github.com/robertbrem/football'
+            git url: 'https://github.com/AdrianJetzer/football'
             container('maven') {
                 sh 'mvn clean package'
             }
@@ -29,8 +29,8 @@ podTemplate(label: 'mypod', containers: [
             sh "git config user.email \"jenkins@khinkali.ch\""
             sh "git config user.name \"Jenkins\""
             sh "git tag -a ${env.VERSION} -m \"${env.VERSION}\""
-            withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/robertbrem/football.git --tags"
+            withCredentials([usernamePassword(credentialsId: 'github78', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/AdrianJetzer/football.git --tags"
             }
 
             container('docker') {
